0233eb28a7ca261c6189f790e0313a54938d153e
Daniel Beeke <daniel@studiofonkel.nl>
2016-05-10 12:27:16 +0200

    Built from 2ba1e60 on branch master

---
+++ js/fix-iframes.js
@@ -0,0 +1,11 @@
+// Modified from markdown.js from Hakim to handle external html files
+(function () {
+    /*jslint loopfunc: true, browser: true*/
+    /*globals alert*/
+    'use strict';
+
+    $('iframe').load(function(){
+        console.log('test')
+    });
+
+})();
